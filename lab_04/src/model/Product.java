9c596ef817ba8fa3032744d9d37530e64df7ecac
Aditi Maurya <maurya.ad@northeastern.edu>
2024-10-12 01:31:02 -0400

    start

---
+++ lab_04/src/model/Product.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author aditi
+ */
+public class Product {
+    private String name;
+    private int price;
+    private int id;
+    
+    private ArrayList<Feature> features;
+    private static int count=0;
+    public Product(){
+        count++;
+        id= count;
+        
+        features = new ArrayList<Feature>();
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+    
+}
---
+++ lab_04/src/model/Supplier.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author aditi
+ */
+public class Supplier {
+    private String supplyName;
+    private ProductCatalog productCatalog;
+    private ImageIcon logoImage;
+    public String getSupplyName() {
+        return supplyName;
+    }
+
+    public void setSupplyName(String supplyName) {
+        this.supplyName = supplyName;
+    }
+
+    public ProductCatalog getProductCatalog() {
+        return productCatalog;
+    }
+
+    public void setProductCatalog(ProductCatalog productCatalog) {
+        this.productCatalog = productCatalog;
+    }
+
+    public ImageIcon getLogoImage() {
+        return logoImage;
+    }
+
+    public void setLogoImage(ImageIcon logoImage) {
+        this.logoImage = logoImage;
+    }
+
+ 
+}
